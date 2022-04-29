2d848304ae139c35597d7d5a0b0746d712fa12e1
Zack Balbin <zack@zackbalbin.com>
2022-04-28 20:39:15 -0700

    start v0.2 functions

--- index.js
+++ index.js
@@ -4,25 +4,64 @@ const EXPLORER_API_URL = "https://api-testnet.ergoplatform.com/";
 const MINT_ADDRESS = "3WwKzFjZGrtKAV7qSCoJsZK9iJhLLrUa3uwd4yw52bVtDVv6j5TL";
 
 class Token {
-    constructor(id, boxId, emmissionAmount, name, description, type, decimals) {
+    constructor(id, boxId, name) {
         this.id = id;
         this.boxId = boxId;
-        this.emmissionAmount = emmissionAmount;
         this.name = name;
-        this.description = description;
-        this.type = type;
-        this.decimals = decimals;
     }
 }
 
-export async function get_token_data(tokenName, limit, offset) {
+async function get_address_data(address) {
+    let url = EXPLORER_API_URL + "api/v1/addresses/" + address + "/balance/confirmed";
+    return await fetch(url)
+        .then(res => res.json())
+        .then(data => { return data })
+}
+
+async function create_address_data(address) {
+    let tokensRaw = await get_address_data(address);
+    let tokens = tokensRaw["tokens"];
+    return tokens;
+}
+
+async function create_address_tokens_array(tokenData) {
+    let tokenArray = []
+    for (let i=0; i<Object.keys(tokenData).length; i++) {
+        let tk = new Token(tokenData[i]["tokenId"], "none", tokenData[i]["name"]);
+        tokenArray.push(tk);
+    }
+    return tokenArray;
+}
+
+async function remove_wrong_names_tokens(tokenArray) {
+    let newArr = []
+    for (let i=0; i<tokenArray.length; i++) {
+        if (tokenArray[i].name[0] ==  "~") {
+            newArr.push(tokenArray[i]);
+        }
+    }
+    return newArr;
+}
+
+async function check_correct_ownership(tokenArray, address) {
+    let ownedErgoNames = [];
+    for (let i=0; i<tokenArray.length; i++) {
+        let ownerAddress = await resolve_ergoname(tokenArray[i].name);
+        if (ownerAddress == MINT_ADDRESS) {
+            ownedErgoNames.push(tokenArray[i]);
+        }
+    }
+    return ownedErgoNames;
+}
+
+async function get_token_data(tokenName, limit, offset) {
     let url = EXPLORER_API_URL + "api/v1/tokens/search?query=" + tokenName + "&limit=" + limit + "&offset=" + offset;
     return await fetch(url)
         .then(res => res.json())
         .then(data => { return data} )
 }
 
-export async function create_token_data(tokenName) {
+async function create_token_data(tokenName) {
     let totalRaw = await get_token_data(tokenName, 1, 0);
     let total = totalRaw['total'];
     let neededCalls = Math.floor(total / 500) + 1;
@@ -41,30 +80,30 @@ export async function create_token_data(tokenName) {
     }
 }
 
-export async function convert_token_data_to_token(data) {
+async function convert_token_data_to_token(data) {
     let tokenArray = []
     for (let i=0; i<Object.keys(data).length; i++) {
-        let tk = new Token(data[i]['id'], data[i]['boxId'], data[i]['emmissionAmount'], data[i]['name'], data[i]['description'], data[i]['type'], data[i]['decimals']);
+        let tk = new Token(data[i]['id'], data[i]['boxId'], data[i]['name']);
         tokenArray.push(tk);
     }
     return tokenArray;
 }
 
-export async function get_box_address(boxId) {
+async function get_box_address(boxId) {
     let url = EXPLORER_API_URL + "api/v1/boxes/" + boxId;
     return await fetch(url)
         .then(res => res.json())
         .then(data => { return data['address']} )
 }
 
-export async function check_box_address(address) {
+async function check_box_address(address) {
     if (address == MINT_ADDRESS) {
         return true;
     }
     return false;
 }
 
-export async function get_asset_minted_at_address(tokenArray) {
+async function get_asset_minted_at_address(tokenArray) {
     for (let i=0; i<tokenArray.length; i++) {
         let address = await get_box_address(tokenArray[i].boxId);
         if (await check_box_address(address)) {
@@ -74,7 +113,7 @@ export async function get_asset_minted_at_address(tokenArray) {
     return null;
 }
 
-export async function get_token_transaction_data(tokenId) {
+async function get_token_transaction_data(tokenId) {
     let total = await get_max_transactions_for_token(tokenId);
     let url = EXPLORER_API_URL + "api/v1/assets/search/byTokenId?query=" + tokenId + "&limit=1&offset=" + (total-1);
     return await fetch(url)
@@ -82,18 +121,18 @@ export async function get_token_transaction_data(tokenId) {
         .then(data => { return data['items']} )
 }
 
-export async function get_max_transactions_for_token(tokenId) {
+async function get_max_transactions_for_token(tokenId) {
     let url = EXPLORER_API_URL + "api/v1/assets/search/byTokenId?query=" + tokenId + "&limit=1";
     return await fetch(url)
         .then(res => res.json())
         .then(data => { return data['total']} )
 }
 
-export async function get_last_transaction(data) {
+async function get_last_transaction(data) {
     return data[data.length - 1];
 }
 
-export async function get_box_id_from_transaction_data(data) {
+async function get_box_id_from_transaction_data(data) {
     return data['boxId'];
 }
 
@@ -110,4 +149,20 @@ export async function resolve_ergoname(name) {
     else {
         return null;
     }
+}
+
+export async function check_already_registered(name) {
+    let address = await resolve_ergoname(name);
+    if (address != null) {
+        return true;
+    }
+    return false;
+}
+
+export async function reverse_search(address) {
+    let tokenData = await create_address_data(address);
+    let tokenArray = await create_address_tokens_array(tokenData);
+    tokenArray = await remove_wrong_names_tokens(tokenArray);
+    let owned = await check_correct_ownership(tokenArray, address);
+    return owned;
 }
